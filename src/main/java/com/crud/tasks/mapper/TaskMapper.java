b780ab94b1bcb79e7882c4d577f2d89a31818fd2
Maciej Olszewski <maciek.ol@op.pl>
2023-09-30 21:28:01 +0200

    poprawki

--- src/main/java/com/crud/tasks/mapper/TaskMapper.java
+++ src/main/java/com/crud/tasks/mapper/TaskMapper.java
@@ -2,11 +2,10 @@ package com.crud.tasks.mapper;
 
 import com.crud.tasks.domain.Task;
 import com.crud.tasks.domain.TaskDto;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-@Component
+
 @Service
 public class TaskMapper {
 
